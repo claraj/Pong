61479fbe71a68392172e6cfd88a21113b6e8282b
clara <clara@clara.com>
2015-01-07 12:13:38 -0600

    Class version. No paddle speed factor.

--- Main.java
+++ Main.java
@@ -7,19 +7,23 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
+
+//TODO have paddle speed affect ball's direction
+//TODO known issue - sometimes ball gets stuck behind human paddle
+
 public class Main {
 
     static int screenSize = 300;    //and width - screen is square
     static int paddleSize = 25;     //Actually half the paddle size - how much to draw on each side of center
     static int paddleDistanceFromSide = 10;  //How much space between each paddle and side of screen
     
-    static int gameSpeed = 100;  //How many milliseconds between clock ticks? Reduce this to speed up game
+    static int gameSpeed = 75;  //How many milliseconds between clock ticks? Reduce this to speed up game
     
     static int computerPaddleY = screenSize / 2 ;    //location of the center of the paddles on the Y-axis of the screen
     static int humanPaddleY = screenSize / 2 ;
     
-    static int computerPaddleMaxSpeed = 10;   //Max number of pixels that paddle can move clock tick
-    static int humanPaddleMaxSpeed = 5;   //This doesn't quite do the same thing...
+    static int computerPaddleMaxSpeed = 3;   //Max number of pixels that computer paddle can move clock tick. Higher number = easier for computer
+    static int humanPaddleMaxSpeed = 5;   //This doesn't quite do the same thing... this is how many pixels human moves per key press TODO use this in a better way
     
     static int humanPaddleSpeed = 0;      // "speed" is pixels moved up or down per clock tick
     static int computerPaddleSpeed = 0;   // same
@@ -31,12 +35,14 @@ public class Main {
     static double ballSpeed = 5;   //Again, pixels moved per clock tick
 
 
-    //An angle in radians. This starts the ball moving down toward the human. Replace with some of the other
+    //An angle in radians (which range from 0 to 2xPI (0 to about 6.3).
+    //This starts the ball moving down toward the human. Replace with some of the other
     //commented out versions for a different start angle
-    //static double ballDirection = 1;   //set this to whatever you want (but helps if you angle towards a player)
+    //set this to whatever you want (but helps if you angle towards a player)
+    static double ballDirection = Math.PI + 1;   //heading left and up
+    //static double ballDirection = 1;
     //static double ballDirection = 0;   //heading right
     //static double ballDirection = Math.PI;   //heading left
-    static double ballDirection = Math.PI + 1;   //heading left and up
 
     static Timer timer;    //Ticks every *gameSpeed* milliseconds. Every time it ticks, the ball and computer paddle move
 
