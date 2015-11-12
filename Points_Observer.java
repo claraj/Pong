8af0b4d5398d99179c56d3d5eae9e18f825ff08d
Malcolm Leehan <kx1863tg@go.minneapolis.edu>
2015-11-12 11:26:04 -0600

    Update Points_Observer.java

--- Points_Observer.java
+++ Points_Observer.java
@@ -2,7 +2,7 @@ import java.util.Observable;
 import java.util.Observer;
 
 /**
- * Created by Malcolm on 11/1/2015.
+ * Created by Malcolm on 11/5/2015.
  */
 public  class Points_Observer implements Observer{
 
@@ -10,13 +10,33 @@ public  class Points_Observer implements Observer{
         if((( Ball)ball).getPointScored()){
             if(Ball.getBallX()<=0){
                 System.out.println("Human Scored!");
+                ((Ball) ball).setBallY(GameDisplay.screenSize/2);
+                ((Ball) ball).setBallX(GameDisplay.screenSize/2);
                 Main.setHumanScore();
+                
             }
             else if(Ball.getBallX()>=GameDisplay.screenSize){
                 System.out.println("Computer Scored!");
+                ((Ball) ball).setBallY(GameDisplay.screenSize/2);
+                ((Ball) ball).setBallX(GameDisplay.screenSize/2);
                 Main.setCompScore();
+                
             }
         }
         //TODO put a observer here. This looks at the ballX Varioable
     }
+
+
+
+
+
+
+    /*class EggAlert implements Observer {
+
+    public void update(Observable chicken, Object arg) {
+        if (((Chicken)chicken).laidEgg() ){
+            System.out.println(chicken + " has laid an egg");
+        }
+
+    }*/
 }
